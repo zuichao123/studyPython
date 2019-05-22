60ac9d2c115430b8b475477687fb9b08953cb2a4
zbql123@163.com <zbql123@163.com>
2019-05-22 23:16:07 +0800

    update in 2019-05-22 23:15:00

--- sunjian/studys/database/mongo/tMongo.py
+++ sunjian/studys/database/mongo/tMongo.py
@@ -2,5 +2,30 @@
 """
     mongodb练习
 """
-import pymongo
+import pymongo as pm
 
+# 建立连接并创建客户端
+# 无安全认证
+client = pm.MongoClient('localhost',27017)
+# 有安全认证
+# client = MongoClient('mongodb://root:root@localhost:27017/sunjian')
+# 切换数据库
+db = client.sunjian
+# 获取集合
+stb = db.stb
+
+# 插入数据
+# stb.insert_one({'name':'sunjian2'})
+
+# 修改
+# stb.update_one({'name':'sunjian2'},{'$set':{'name':'sunjian3'}})
+
+# 删除
+# stb.delete_one({'name':'sunjian3'})
+
+# 获取数据信息
+datas = stb.find()
+
+# 输出信息
+for data in datas:
+    print(data['name'])
---
+++ sunjian/studys/database/redis/tredis.py
@@ -0,0 +1,4 @@
+# coding:utf-8
+"""
+    redis学习
+"""
