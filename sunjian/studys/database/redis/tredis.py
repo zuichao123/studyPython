664d424bdaf75a3da8d53c6140e56cd3f8f3cd34
sdf <asdf>
2019-05-23 20:11:21 +0800

    update in 2019-05-22 09:18:30

--- sunjian/studys/database/redis/tredis.py
+++ sunjian/studys/database/redis/tredis.py
@@ -2,3 +2,10 @@
 """
     redis学习
 """
+import redis
+
+r = redis.StrictRedis(host='127.0.0.1', port=6379)
+r.set('foo', 'hello')
+r.rpush('mylist', 'one')
+print (r.get('foo'))
+print (r.rpop('mylist'))
