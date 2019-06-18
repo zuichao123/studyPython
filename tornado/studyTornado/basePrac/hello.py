ea2ce1ed024fb647c28e770f5a168f282a92aec9
sdf <asdf>
2019-06-18 17:43:15 +0800

    update in 2019-06-17 09:59:30

---
+++ tornado/studyTornado/basePrac/hello.py
@@ -0,0 +1,21 @@
+# coding:utf-8
+"""
+
+"""
+import tornado.web
+import tornado.ioloop
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    """主页处理类"""
+
+    def get(self):
+        """对应http的get请求方式"""
+        self.write("hello sunjian!")
+
+
+
+if __name__ == "__main__":
+    app = tornado.web.Application([(r"/", IndexHandler)]) # 实例化一个服务器对象，映射
+    app.listen(9000) # 监听端口
+    tornado.ioloop.IOLoop.current().start()
