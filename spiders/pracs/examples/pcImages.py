da7a5cf1b078f055a19608211fc3d9a2da079e43
sdf <asdf>
2019-07-17 13:59:42 +0800

    update in 2019-07-17 13:59:30

--- spiders/pracs/examples/pcImages.py
+++ spiders/pracs/examples/pcImages.py
@@ -1,16 +1,11 @@
 #coding:utf-8
 """
-    爬虫爬取指定页面的后缀为.jpg的图片
+    爬虫爬取指定页面的图片
 """
-import codecs
-import random
 import re
-import sys
 import urllib
 import urllib.request
 
-#获取页面的源代码
-from imp import reload
 from urllib.parse import quote
 
 class PcImage(object):
@@ -18,28 +13,24 @@ class PcImage(object):
     # 获取网页源代码
     def getHtml(self, url):
         headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36"
+            'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'
         }
-        request = urllib.request.Request(url, headers=headers)#打开URL地址
-        page = urllib.request.urlopen(request, timeout=10)
-        html = page.read()#读取页面源代码信息
-        return html#返回信息
+        request = urllib.request.Request(url, headers=headers) # 打开URL地址
+        page = urllib.request.urlopen(request, timeout=10) # 请求页面返回页面对象
+        html = page.read() # 读取页面源代码信息
+        return html # 返回信息
 
     # 下载图片
     def getImg(self, html, reg, path):
-        imgre = re.compile(reg)#将正则规则编译
-        imglist = re.findall(imgre,html.decode('utf-8'))#从HTML中匹配正则
-        # imglist = re.findall(imgre,str(html))#从HTML中匹配正则
-        #遍历匹配到的正则信息
-        x = 48#定义变量给图片命名
-        reg2 = r'denglu/(.*?)\.png'
-        imgre2 = re.compile(reg2)
-        for imgurl in imglist:
-            imgurl = "https://axhub.im/pro/84103267b2434f03/"+imgurl
-            imgurl = quote(imgurl,safe='/:?=')
+        imgre = re.compile(reg) # 将正则规则编译
+        imglist = re.findall(imgre,html.decode('utf-8')) # 从HTML中匹配正则
+        # imglist = re.findall(imgre,str(html)) # 从HTML中匹配正则
+
+        x = 48 # 定义变量给图片命名
+        for imgurl in imglist: # 遍历匹配到的正则信息
+            imgurl = quote(imgurl,safe='/:?=') # 处理URL中包含中文
             print('----------->>' + imgurl)
-            urllib.request.urlretrieve(imgurl,path+'%s.jpg' %x)#py3的urlopen返回的不是string是bytes，需要将html中的html.decode('utf-8')
-            # urllib.request.urlretrieve(imgurl,"%s.jpg"%x)#py3的urlopen返回的不是string是bytes，需要将html中的html.decode('utf-8')
+            urllib.request.urlretrieve(imgurl, path+'%s.jpg' %x) # py3的urlopen返回的不是string是bytes，需要将html中的html.decode('utf-8')
             x+=1
 
     # 练习URL中有中文
@@ -57,20 +48,24 @@ class PcImage(object):
         imgurl = "https://axhub.im/pro/84103267b2434f03/"+imgurl
         imgurl = quote(imgurl,safe='/:?=')
         print('----------->>' + imgurl)
-        urllib.request.urlretrieve(imgurl,'C:\\Users\\sunjian\\Desktop\\Zbpl\\Data\\image\\s.jpg')#py3的urlopen返回的不是string是bytes，需要将html中的html.decode('utf-8')
+        urllib.request.urlretrieve(imgurl, 'C:\\Users\\sunjian\\Desktop\\Zbpl\\Data\\image\\s.jpg')#py3的urlopen返回的不是string是bytes，需要将html中的html.decode('utf-8')
+
+    # start..
+    def main(self):
+        url = "http://www.win4000.com/meinvtag59_1.html"
+        reg = r'src="(.*?\.jpg)"'
+
+        # url = "https://axhub.im/pro/84103267b2434f03/denglu.html"
+        # reg = r'class="img " src="(.*?\.png)"'
+
+        savePath = 'C:\\Users\\sunjian\\Desktop\\Zbpl\\Data\\image\\'
+
+        html = self.getHtml(url)
+        self.getImg(html, reg, savePath)
 
 
 if __name__ == '__main__':
     pcImage = PcImage()
+    pcImage.main()
 
-    url = "http://www.win4000.com/meinvtag59_1.html"
-    reg = r'src="(.*?\.jpg)"'
-
-    # url = "https://axhub.im/pro/84103267b2434f03/denglu.html"
-    # reg = r'class="img " src="(.*?\.png)"'
-
-    savePath = 'C:\\Users\\sunjian\\Desktop\\Zbpl\\Data\\image\\'
-
-    html = pcImage.getHtml(url)#获取该地址的所有源代码
-    pcImage.getImg(html, reg, savePath)
 
